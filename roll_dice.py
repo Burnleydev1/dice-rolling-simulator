3f799e0b64c12136d0058fefa533af8bb0a755ec
Abongwa Bonalais Amahnui <abongwabonalais@gmail.com>
2022-05-29 11:53:16 +0100

    Minor Changes

--- roll_dice.py
+++ roll_dice.py
@@ -1,5 +1,55 @@
 import random
 
+DICE_ART = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+}
+DIE_HEIGHT = len(DICE_ART[1])
+DIE_WIDTH = len(DICE_ART[1][0])
+DIE_FACE_SEPARATOR = " "
+
+
+
 
 def parse_input(input_string):
     """
@@ -33,7 +83,9 @@ def roll_dice(num_dice):
         for _ in range(num_dice):
             roll = random.randint(1, 6)
             roll_results.append(roll)
-        return roll_results
+
+
+
 
 
 num_dice_input = input("How many dice would you like to roll?[1-6] ")
