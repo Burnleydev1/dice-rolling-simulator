608f87f55544ef3f000f03c247f201f0c89770fc
Abongwa Bonalais Amahnui <abongwabonalais@gmail.com>
2022-05-29 12:43:50 +0100

    Minor changes

--- roll_dice.py
+++ roll_dice.py
@@ -83,10 +83,11 @@ def roll_dice(num_dice):
         for _ in range(num_dice):
             roll = random.randint(1, 6)
             roll_results.append(roll)
+    return roll_results
 
 
 
-def display_dice(dice_values):
+def generate_dice_faces_diagram(dice_values):
     dice_faces = []
     for value in dice_values:
         dice_faces.append(DICE_ART[value])
@@ -100,12 +101,15 @@ def display_dice(dice_values):
         dice_faces_rows.append(row_string)
 
     width = len(dice_faces_rows[0])
-    diagram_header = "RESULTS " .center(width, "=")
+    diagram_header = "RESULTS ".center(width, "=")
 
-    dice_faces_diagram = "/n".join[diagram_header] + dice_faces_rows
+    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+    return dice_faces_diagram
 
 
 
 num_dice_input = input("How many dice would you like to roll?[1-6] ")
 num_dice = parse_input(num_dice_input)
 roll_results = roll_dice(num_dice)
+dice_face_diagram = generate_dice_faces_diagram(roll_results)
+print(f"\n{dice_face_diagram}")
